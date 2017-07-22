7895bc854ae0d4a7c64d4d46ee481cc6f97542de
DevHN <devhn0@gmail.com>
2017-07-22 14:03:14 +0800

    Added contents
    - Added some features' structure experimentally in Commands.java

--- src/com/devhn/plugin/Commands/Commands.java
+++ src/com/devhn/plugin/Commands/Commands.java
@@ -9,10 +9,27 @@ import org.bukkit.command.CommandSender;
  */
 
 public class Commands implements CommandExecutor {
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String s, String[] strings) {
-        boolean isOp = sender.isOp();
 
+    private static final String PREFIX = "§c[ §4§li §c] ";
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
+        boolean isOp = sender.isOp();
+        switch (args.length) {
+            case 1:
+                if (!isOp) {
+                    if (args[0].equalsIgnoreCase("help")) {
+                        sender.sendMessage(PREFIX + "§f§l도움말 -");
+                        sender.sendMessage("");
+                    } else if (args[0].equalsIgnoreCase("info")) {
+                        sender.sendMessage(PREFIX + "§f§l정보 -");
+                        sender.sendMessage("- §7이 §3§l플러그인§7은 §3§l개발자 DevHN§7가 제작하였습니다.");
+                    }
+                }
+                break;
+            case 2:
+            default:
+        }
         return true;
     }
 }
