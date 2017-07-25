c5113495ebd286276daec73fc60c91f9cd73a038
DevHN <devhn0@gmail.com>
2017-07-25 13:10:31 +0800

    Added function
    - Added function of adding lore to Lore.java

--- src/com/devhn/plugin/function/Lore.java
+++ src/com/devhn/plugin/function/Lore.java
@@ -1,8 +1,37 @@
 package com.devhn.plugin.function;
 
+import org.bukkit.command.CommandSender;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.material.MaterialData;
+
+import java.util.List;
+
 public class Lore {
-    public String addLore() {
-        return "";
+
+    public static String tempStr;
+
+    public static String setTempStr(String str) {
+        return str = tempStr;
+    }
+
+    public void addLore(int ID, int DATA, int STACK, List<String> Lore, CommandSender sender) {
+        try {
+            Integer.parseInt(String.valueOf(tempStr));
+
+            ItemStack itemStack = new MaterialData(ID, (byte) DATA).toItemStack(STACK);
+            ItemMeta itemMeta = itemStack.getItemMeta();
+
+            itemMeta.setLore(Lore);
+
+        } catch (NumberFormatException e) {
+            sender.sendMessage("§c[ §4§l! §c] §3§l줄 수§7를 입력해주세요.");
+            return;
+        }
+    }
+
+    public void removeLore() {
+
     }
 
     public boolean isContaintedLore() {
