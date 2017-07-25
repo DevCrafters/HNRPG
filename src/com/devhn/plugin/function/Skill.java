e02a39a81c99500b0f098525f3f4b4a43652d572
DevHN <devhn0@gmail.com>
2017-07-25 13:10:52 +0800

    Created new class
    - Created new class

---
+++ src/com/devhn/plugin/function/Skill.java
@@ -0,0 +1,15 @@
+package com.devhn.plugin.function;
+
+public class Skill {
+    public void addSkill() {
+
+    }
+
+    public void removeSkill() {
+
+    }
+
+    public boolean isContainedSkill() {
+        return true;
+    }
+}
