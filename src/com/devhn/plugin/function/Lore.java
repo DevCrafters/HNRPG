862cadfe83e8066987da2d245e972c259aaf23d5
DevHN <devhn0@gmail.com>
2017-08-28 21:14:42 +0800

    Delete import setting
    - Delete import setting on Lore.java

--- src/com/devhn/plugin/function/Lore.java
+++ src/com/devhn/plugin/function/Lore.java
@@ -5,7 +5,6 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.material.MaterialData;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Lore {
