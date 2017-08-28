0b9bfa7756f6bcf52f091e8d424ab578e2a2a0c1
DevHN <devhn0@gmail.com>
2017-08-28 21:14:24 +0800

    Add contents
    - Add contents in Item.java

--- src/com/devhn/plugin/function/Item.java
+++ src/com/devhn/plugin/function/Item.java
@@ -1,4 +1,19 @@
 package com.devhn.plugin.function;
 
 public class Item {
+    public void addItem() {
+
+    }
+
+    public void removeItem() {
+
+    }
+
+    public void editItem() {
+
+    }
+
+    public void getItem() {
+
+    }
 }
